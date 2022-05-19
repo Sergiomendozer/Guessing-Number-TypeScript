98a3f8f5971e08828fd7314cd157723e0ccb50ee
sergiomendozer <sergiomendozansx@gmail.com>
2022-05-19 13:46:47 -0700

    did they guess it function

--- Guess_it.ts
+++ Guess_it.ts
@@ -5,18 +5,22 @@
 // const prompt = require("prompt-sync")({ sigint: true });
 // const n: number = prompt("You are going to guess a number from 0 to :");
 // console.log(`Your number is from zero to ${n}.`);
+function did_they_guess_it(n: number, r: number, guess: number): void {
+  console.log("called");
+}
 
 function guess_it(n: number, r: number): void {
-  //   let random_number: number = Math.floor(Math.random() * 100 + 0);
-  //   console.log("R: " + random_number);
   console.log("N:" + n);
   console.log("R:" + r);
+  const prompt = require("prompt-sync")({ sigint: true });
+  let guess: number = prompt("Take a guess: ");
+  console.log("G:" + guess);
+  return did_they_guess_it(n, r, guess);
 }
 
 function random_number_generator(n: number): void {
   let random_number: number = Math.floor(Math.random() * n + 0);
-  //   console.log("R: " + random_number); // for testing
-  guess_it(n, random_number);
+  return guess_it(n, random_number);
 }
 //! constant n is just for testing
 const n: number = 100;
