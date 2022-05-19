5436832b5d2352078a416c4040dbcf27db0b7277
sergiomendozer <sergiomendozansx@gmail.com>
2022-05-19 13:28:28 -0700

    two seprate functions

--- Guess_it.ts
+++ Guess_it.ts
@@ -6,11 +6,18 @@
 // const n: number = prompt("You are going to guess a number from 0 to :");
 // console.log(`Your number is from zero to ${n}.`);
 
-function guess_it(n: number): number {
-  let random_number: number = Math.floor(Math.random() * 100 + 0);
-  console.log("R: " + random_number);
-  return random_number;
+function guess_it(n: number, r: number): void {
+  //   let random_number: number = Math.floor(Math.random() * 100 + 0);
+  //   console.log("R: " + random_number);
+  console.log("N:" + n);
+  console.log("R:" + r);
+}
+
+function random_number_generator(n: number): void {
+  let random_number: number = Math.floor(Math.random() * n + 0);
+  //   console.log("R: " + random_number); // for testing
+  guess_it(n, random_number);
 }
 //! constant n is just for testing
 const n: number = 100;
-console.log(guess_it(n));
+random_number_generator(n);
