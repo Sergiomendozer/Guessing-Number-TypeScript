f3209d066f5fc235bd8e86ef8b48bed8cd0de51d
sergiomendozer <sergiomendozansx@gmail.com>
2022-05-19 13:21:16 -0700

    random number generater create

--- Guess_it.ts
+++ Guess_it.ts
@@ -1,13 +1,16 @@
 // let firstName: string = "Dylan";
 // console.log(firstName);
 // ts-node Guess_it
+
 // const prompt = require("prompt-sync")({ sigint: true });
 // const n: number = prompt("You are going to guess a number from 0 to :");
 // console.log(`Your number is from zero to ${n}.`);
 
-function multiply(n: number): number {
-  return n;
+function guess_it(n: number): number {
+  let random_number: number = Math.floor(Math.random() * 100 + 0);
+  console.log("R: " + random_number);
+  return random_number;
 }
 //! constant n is just for testing
 const n: number = 100;
-console.log(multiply(n));
+console.log(guess_it(n));
