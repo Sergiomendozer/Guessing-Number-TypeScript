126d7e941d6bd282238c839eac6346d40d7c40e2
sergiomendozer <sergiomendozansx@gmail.com>
2022-05-18 22:37:40 -0700

    set up Ts for coding

---
+++ Guess_it.ts
@@ -0,0 +1,12 @@
+// let firstName: string = "Dylan";
+// console.log(firstName);
+// ts-node Guess_it
+// const prompt = require("prompt-sync")({ sigint: true });
+// const n: number = prompt("You are going to guess a number from 0 to :");
+// console.log(`Your number is from zero to ${n}.`);
+
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+// if console log ==> put void for return
+console.log(multiply(5, 2));
---
+++ notes.py
@@ -0,0 +1,5 @@
+#  npm install -g typescript
+# npx tsc
+# or 
+# npm install -g ts-node
+# ts-node {file-name}
